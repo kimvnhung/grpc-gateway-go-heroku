b07adb14bcfc2e8e787278287c130b8050c2d536
kimvnhung <kimvnhung@gmail.com>
2022-08-04 15:40:37 +0700

    fix

--- main.go
+++ main.go
@@ -1,34 +1,108 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
-	"time"
 
-	"github.com/gin-gonic/gin"
 	_ "github.com/gin-gonic/gin"
 	_ "github.com/heroku/x/hmetrics/onload"
+	pb "hung.kv/protogenerated"
 )
 
+var (
+	grpcServerEndpoint = flag.String("server-port", "1998", "gRPC server port")
+	grpcGatewayPort    = flag.String("gateway-port", "1997", "Gateway port")
+)
+
+type server struct {
+	pb.UnimplementedSimulatorServer
+}
+
+func (s *server) Home(ctx context.Context, in *pb.Empty) (*pb.HelloReply, error) {
+	log.Printf("Home")
+	return &pb.HelloReply{Message: "Hello World"}, nil
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
 func main() {
+	flag.Parse()
 	port := os.Getenv("PORT")
 
 	if port == "" {
 		log.Fatal("$PORT must be set")
 	}
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
+	*grpcGatewayPort = port
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
-	})
+	err := establishGrpcServer()
 
-	for {
-		time.Sleep(10 * time.Second)
-		log.Println("Running")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	err = establisGrpcGateway()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func establishGrpcServer() error {
+	// Create a listener on TCP port
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%s", *grpcServerEndpoint))
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+
+	// Create a gRPC server object
+	s := grpc.NewServer()
+	// Attach the Greeter service to the server
+	pb.RegisterSimulatorServer(s, &server{})
+	// Serve gRPC server
+	log.Printf("server listening at %v", lis.Addr())
+	go func() {
+		log.Fatalln(s.Serve(lis))
+	}()
+	return nil
+}
+
+func establisGrpcGateway() error {
+	// Create a client connection to the gRPC server we just started
+	// This is where the gRPC-Gateway proxies the requests
+	conn, err := grpc.DialContext(
+		context.Background(),
+		fmt.Sprintf("0.0.0.0:%s", *grpcServerEndpoint),
+		grpc.WithBlock(),
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	// Register Greeter
+	err = pb.RegisterSimulatorHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gwServer := &http.Server{
+		Addr:    fmt.Sprintf(":%s", *grpcGatewayPort),
+		Handler: gwmux,
+	}
+
+	log.Printf("Serving gRPC-Gateway on http://0.0.0.0:%s\n", *grpcGatewayPort)
+	log.Fatalln(gwServer.ListenAndServe())
+
+	return nil
 }
